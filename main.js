d50856e26e656d5b406c1280a3919ab07bac050c
geomaas <geoff.maas@gmail.com>
2016-05-25 17:17:58 -0400

    wed at5

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var charConfirm = confirm("ready to play " + charName + "?");
         alert("home of the ewoks!");
         alert("...but also big-ass talking spiders, cus we're mixing universes...");
         var forestChoice = prompt("Type a to fight 3 mouthy spiders or type b to see if the ewoks will help you");
-          switch (forestChoice.toLowercase()) {
+          switch (forestChoice) {
             case "a" || "A":
               alert("the spiders go on a diatribe of how time is like a 'web' and we are all caught in it.");
               alert("it renders all your attacks useless and you die of boredom.");
@@ -20,19 +20,45 @@ var charConfirm = confirm("ready to play " + charName + "?");
               alert("a sparkling pool of water catches your eye. you bend over to take a drink. You touch the water...");
               alert("...and now you're trapped under the water likes its glass. WTF.");
               alert("You cannot get out. You drown!");
-              alert("Game Over and if you got the 'Ewok Adventure' reference then you're a huge nerd and its a terrible movie")
-              window.open("http://i.imgur.com/bmQOPTL.gif")
+              alert("Game Over and if you got the 'Ewok Adventure' reference then you're a huge nerd and its a terrible movie");
+              window.open("http://i.imgur.com/bmQOPTL.gif");
             default:
+              alert("You can't follow instructions so spider-riding Ewoks poke you to death while singing 'nub-nub'");
 
           }
         break;
       case "mountains":
-        alert("I dunno trolls or something... Yeah the kind with the pointy hair");
-        alert("You brush their hair and become a god of sorts");
-        var mounatinChoice = prompt("Type ")
+        alert("I dunno you find trolls or something... Yeah the kind with the pointy hair...");
+        alert("You brush their hair and they offer you to become a god of sorts");
+        var mounatinChoice = prompt("Type a to accept this mantle of responsibility, type b to run away");
+          switch (mounatinChoice) {
+            case "a":
+              alert("You are Lisa Frank Reborn!!!");
+              alert("May the world tremble at your multi-colored trapper-keepers!!!");
+              alert("But Game Over cus I said so")
+              window.open("https://media.giphy.com/media/FTEfzFmrBkbqU/giphy.gif");
+              break;
+            case "b":
+              alert("The trolls are displeased with your selfish choice.");
+              alert("They place a curse on you");
+              alert("Many years later drown in the shower because the drain was clogged with HAIR!")
+              alert("U ded, u lose.");
+              window.open("https://www.youtube.com/watch?v=BqeQmkI7tFI")
+            default:
+              alert("a or b dummy. Its not that hard you lose.");
+
+          }
       case "dessert":
         alert("You basically made it to candy-land, you squanchy mother-f@*#er!");
         alert("sorry been watching a lot of 'Rick and Morty' lately...");
+        var dessertChoice = prompt("You like candy right?");
+          if (dessertChoice === "yes") {
+            alert("you got the diabeetus!");
+            window.open("https://media.giphy.com/media/KXlv7s9XOReLe/giphy.gif");
+          }else {
+            alert("you still got the diabeetus. Because I'm a lazy writer.");
+            window.open("https://media.giphy.com/media/KXlv7s9XOReLe/giphy.gif");
+          }
 
       default:
         alert("No its not desert. I meant what I meant. Also if you put anything else you a dumb-dumb and ended the game.")
@@ -40,7 +66,7 @@ var charConfirm = confirm("ready to play " + charName + "?");
         alert("Ok, you can go now...")
         setTimeout(function(){ alert("Just kidding... You Win!!!!"); }, 3000);
         // setTimeout(window.open("http://usvsth3m.com/wp-content/uploads/2013/08/nHyjdUb.gif"),2000);
-        setTimeout(function() { window.open("http://usvsth3m.com/wp-content/uploads/2013/08/nHyjdUb.gif") }, 5000);
+        setTimeout(function() { window.open("http://usvsth3m.com/wp-content/uploads/2013/08/nHyjdUb.gif") }, 4000);
     }
   }else {
    alert("have a good day");
